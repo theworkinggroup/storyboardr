0044eb7c7cb925db8f690ad0061b85663e54520b
Oleg <oleg@theworkinggroup.ca>
2010-04-23 22:53:13 -0400

    all formatting seems to be working

--- storyboard.rb
+++ storyboard.rb
@@ -35,14 +35,14 @@ class Storyboard < Sinatra::Base
       stories_line.each do |story|
         row << [
           [ Prawn::Table::Cell.make(@pdf, rendered_story(story), :width => 295) ],
-          [ Prawn::Table::Cell.make(@pdf, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', :width => 295) ],
-          [ Prawn::Table::Cell.make(@pdf, rendered_footer, :width => 295)]
+          [ Prawn::Table::Cell.make(@pdf, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', :width => 295, :border_color => 'cccccc', :font_size => 9, :text_color => 'ff0000', :background_color => '00ff00') ],
+          [ Prawn::Table::Cell.make(@pdf, rendered_footer, :width => 295, :border_color => 'cccccc', :height => 50)]
         ]
       end
       table << row 
     end
     
-    @pdf.table( table )
+    @pdf.table( table, :cell_style => { :border_color => 'cccccc' } )
     
     content_type 'application/pdf'
     attachment 'test.pdf'
